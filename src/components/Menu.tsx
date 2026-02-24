9f574c318a8f0647cabf22b83cd1ca1c58924fab
전세혁 <jktpgur@hivelab.co.kr>
2026-02-24 12:10:28 +0900

    해시태그 검색 구현

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,11 @@ import { BiUserCircle } from "react-icons/bi";
 import { MdLogout, MdLogin } from "react-icons/md";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
+import { getAuth, signOut } from "firebase/auth";
+import { app } from "firebaseApp";
 import AuthContext from "context/AuthContext";
+import { AiOutlineSearch } from "react-icons/ai";
+import { toast } from "react-toastify";
 
 export default function MenuList(){
   const { user } = useContext(AuthContext);
@@ -19,13 +23,24 @@ export default function MenuList(){
           <BiUserCircle />
           Profile
         </button>
+        <button type="button" onClick={() => navigate("/search")}>
+          <AiOutlineSearch />
+          Search
+        </button>
         { user === null ? (
           <button type="button" onClick={() => navigate("/users/login")}>
             <MdLogin />
             Login
           </button>
         ): (
-          <button type="button" onClick={() => navigate("/")}>
+          <button
+            type="button"
+            onClick={async () => {
+              const auth = getAuth(app);
+              await signOut(auth);
+              toast.success("로그아웃 되었습니다.");
+            }}
+          >
             <MdLogout />
             Logout
           </button>
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+import PostBox from "components/posts/PostBox";
+import { PostProps } from "pages/home";
+
 export default function SearchPage() {
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  const onChange = () => {};
   return (
-    <h1>Search Page</h1>
-  )
+    <div className="home">
+      <div className="home__top">
+        <div className="home__title">
+          <div className="home__title-text">Search</div>
+        </div>
+        <div className="home__search-div">
+          <input
+            type="text"
+            className="home__search"
+            placeholder="해시태그 검색"
+            onChange={onChange}
+          />
+        </div>
+      </div>
+      <div className="post">
+        {posts?.length > 0 ? (
+          posts?.map((post) => <PostBox post={post} key={post.id} />)
+        ) : (
+          <div className="post__no-posts">
+            <div className="post__text">게시글이 없습니다.</div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
