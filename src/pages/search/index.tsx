dde60417fdd1ba1fb71998b86404853e55c8c75c
전세혁 <jktpgur@hivelab.co.kr>
2026-03-23 16:29:47 +0900

    수정

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,21 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import PostBox from "components/posts/PostBox";
 import { PostProps } from "pages/home";
+import AuthContext from "context/AuthContext";
+import {
+  collection,
+  query,
+  where,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { db } from "firebaseApp";
+
 import useTranslation from "hooks/useTranslation";
 
 export default function SearchPage() {
   const [posts, setPosts] = useState<PostProps[]>([]);
-  const onChange = () => {};
+  const [tagQuery, setTagQuery] = useState<string>("");
+  const { user } = useContext(AuthContext);
   const t = useTranslation();
+
+  const onChange = (e: any) => {
+    setTagQuery(e?.target?.value?.trim());
+  };
+
+  useEffect(() => {
+    if (user) {
+      let postsRef = collection(db, "posts");
+      let postsQuery = query(
+        postsRef,
+        where("hashTags", "array-contains-any", [tagQuery]),
+        orderBy("createdAt", "desc")
+      );
+
+      onSnapshot(postsQuery, (snapShot) => {
+        let dataObj = snapShot?.docs?.map((doc) => ({
+          ...doc?.data(),
+          id: doc?.id,
+        }));
+
+        setPosts(dataObj as PostProps[]);
+      });
+    }
+  }, [tagQuery, user]);
+
   return (
     <div className="home">
       <div className="home__top">
         <div className="home__title">
-          <div className="home__title-text">Search</div>
+          <div className="home__title-text">{t("MENU_SEARCH")}</div>
         </div>
         <div className="home__search-div">
           <input
-            type="text"
             className="home__search"
             placeholder={t("SEARCH_HASHTAG")}
             onChange={onChange}
